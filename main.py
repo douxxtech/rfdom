95b0507c4608984a9b55c7379ffcf56ee3a4f078
douxxtech <douxx@douxx.tech>
2026-02-06 23:58:23 +0100

    updated examples

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ random = RFDom(host="192.168.1.185")
 
 dotrandom = random.random()
 dotuniform = random.uniform(0, 19.69)
+dotrandint = random.randint(0, 20)
+dotrandrange = random.randrange(20)
+dotchoice = random.choice([1, 2, 3, 4, 5, ":3", 69])
 
 print(f".random() -> {dotrandom}")
 print(f".uniform(0, 19.69) -> {dotuniform}")
+print(f".randint(0, 20) -> {dotrandint}")
+print(f".randrange(20) -> {dotrandrange}")
+print(f".choice([1, 2, 3, 4, 5, \":3\", 69]) -> {dotchoice}")
